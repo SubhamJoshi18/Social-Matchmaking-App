65b5a78690b7ff6ae9ba3a14b06d9c57dfc27c2c
SubhamJoshi18 <shubhamrajjoshi69@gmail.com>
2024-12-25 11:19:05 +0545

    add rate limiter for the api

--- node-backend/src/routers/auth.router.ts
+++ node-backend/src/routers/auth.router.ts
@@ -2,15 +2,17 @@ import type, { Router } from 'express';
 import { param, validationResult } from 'express-validator';
 import authController from '../controller/auth.controller';
 import { validateParams } from '../middleware/paramvalidator.middleware';
+import ratelimiter from '../middleware/rateLimit.middleware';
 
 const authRouter: Router = Router();
 
-authRouter.post('/register', authController.registerController);
-authRouter.post('/login', authController.loginUser);
-authRouter.post('/forget-password', authController.forgetPassword);
+authRouter.post('/register', ratelimiter, authController.registerController);
+authRouter.post('/login', ratelimiter, authController.loginUser);
+authRouter.post('/forget-password', ratelimiter, authController.forgetPassword);
 
 authRouter.get(
   '/checkResetPassword/:token/:userId',
+  ratelimiter,
   param('token').isUUID(),
   param('userId').isString(),
   validateParams as any,
@@ -19,6 +21,7 @@ authRouter.get(
 
 authRouter.post(
   '/reset-password/:token/:userId',
+  ratelimiter,
   param('token').isUUID(),
   param('userId').isString(),
   validateParams as any,
