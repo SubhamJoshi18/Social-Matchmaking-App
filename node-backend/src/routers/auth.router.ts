8905fefe8e902017f63c6171816ae427b3a2dc4b
SubhamJoshi18 <shubhamrajjoshi69@gmail.com>
2024-12-25 10:36:19 +0545

    add express validator for the params

--- node-backend/src/routers/auth.router.ts
+++ node-backend/src/routers/auth.router.ts
@@ -1,14 +1,27 @@
 import type, { Router } from 'express';
+import { param, validationResult } from 'express-validator';
 import authController from '../controller/auth.controller';
+import { validateParams } from '../middleware/paramvalidator.middleware';
 
 const authRouter: Router = Router();
 
 authRouter.post('/register', authController.registerController);
 authRouter.post('/login', authController.loginUser);
 authRouter.post('/forget-password', authController.forgetPassword);
+
 authRouter.get(
   '/checkResetPassword/:token/:userId',
+  param('token').isUUID(),
+  param('userId').isString(),
+  validateParams as any,
   authController.checkResetPasswordLink
 );
-authRouter.post('/reset-password/:token/:userId', authController.resetPassword);
+
+authRouter.post(
+  '/reset-password/:token/:userId',
+  param('token').isUUID(),
+  param('userId').isString(),
+  validateParams as any,
+  authController.resetPassword
+);
 export default authRouter;
